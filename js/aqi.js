8543f0e096d6419f6d4ee33c1f1297e5226fafc4
Melvin <melvinkwanys@gmail.com>
2024-06-17 13:56:45 +0800

    grouped everything in aqi.js

--- js/aqi.js
+++ js/aqi.js
@@ -1,5 +1,6 @@
 "use strict";
 
+var map;
 const searchInput = document.querySelector(".searchInput");
 const aqiContainer = document.getElementById("city-aqi-container");
 
@@ -19,7 +20,7 @@ async function fetchAQI() {
             }
 
             const data = await response.json();
-            geocode();
+            geocode(city);
             displayResult(data);
             
         } catch (error) {
@@ -29,7 +30,9 @@ async function fetchAQI() {
     }
     else
     {
+        // city = "Malaysia";
         aqiContainer.innerHTML = '';
+        GetMap();
     }
 }
 
@@ -64,10 +67,9 @@ function debounce(func, wait) {
     };
 }
 
-function geocode(){
+function geocode(city){
     Microsoft.Maps.loadModule('Microsoft.Maps.Search', function () {
         var searchManager = new Microsoft.Maps.Search.SearchManager(map);
-        const city = searchInput.value;
         var requestOptions = {
             bounds: map.getBounds(),
             where: city,
@@ -82,4 +84,27 @@ function geocode(){
         
     });
 }
+
+function GetMap()
+    {
+        map  =  new  Microsoft.Maps.Map(document.getElementById('map'),  {  
+            center:  new  Microsoft.Maps.Location(4.2105, 101.9758), //malaysia latitude,longtitude 
+            zoom:  6,  
+      });
+      
+
+      var  options  =  {  
+            uriConstructor:  "https://tiles.aqicn.org/tiles/usepa-aqi/{zoom}/{x}/{y}.png?token=2545fed08b2dbc8e1d73068f9c6f034e0b9b9f36",  
+            minZoom:  1,  
+            maxZoom:  15  
+      };
+      
+      
+      var  waqiTileSource  =  new  Microsoft.Maps.TileSource(options);  
+      var  waqiTilelayer  =  new  Microsoft.Maps.TileLayer({  mercator:  waqiTileSource  });  
+      map.layers.insert(waqiTilelayer);
+
+      
+
+    }
 
